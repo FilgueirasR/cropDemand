e83b91374f95669b0d78edb7285848c7426aa715
Roberto Filgueiras <betofilgueiras@gmail.com>
2021-01-24 21:23:19 -0300

    Enhancing the download function
    Changes in download function  to better perfomance

--- R/download_terraclimate.R
+++ R/download_terraclimate.R
@@ -29,33 +29,32 @@
 #'                           region = "biomes_brazil",
 #'                           sub_region = 6)
 #' }
-#' @return Load the ppt (Rainfall) or eto (reference evapotranspiration) rasterstack
+#' @return Download for the region of interest the ppt (Rainfall) or eto (reference evapotranspiration) rasterstack
 #' @references
 #' The images used in this package can be found in the paper: Abatzoglou, J.T., S.Z. Dobrowski, S.A. Parks, K.C. Hegewisch, 2018, Terraclimate, a high-resolution global dataset of monthly climate and climatic water balance from 1958-2015, Scientific Data.
 #' @export
 
 download_terraclimate <- function(dir_out, variable, years, region, sub_region){
-  tempdir<- dir_out
-  if(variable == "eto"){variable<-"pet"} else {}
-  outdir<- gsub('\\', '/', tempdir, fixed=TRUE)
-  if(variable == "ppt"| variable == "pet"){
-    for(i in 1:length(years)){
 
+  if(variable == "eto"){variable<-"pet"} else {}
+  if(variable == "ppt"| variable == "pet"){
+
+    for(i in 1:length(years)){
       baseurl <- paste0("http://thredds.northwestknowledge.net:8080/thredds/fileServer/TERRACLIMATE_ALL/data/TerraClimate_", variable, "_", years[i],".nc")
       name_img<- paste0("TerraClimate_", variable, "_", years[i], ".nc")
-      outfile<-paste0(outdir, "/", name_img)
+      outfile<-paste0(dir_out, "/", name_img)
       utils::download.file(url = baseurl, destfile = outfile, mode = "wb")
+      img<-stack(list.files(dir_out, pattern = name_img, full.names = T))
+      invisible(capture.output(shp<-readOGR(system.file('extdata', paste0(region, ".shp"), package= 'cropDemand'))))
+      area<-shp[sub_region,]
+      img<-crop(img, area)
+      img<-mask(img, area)
+      img<-projectRaster(img, crs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+      unlink(outfile)
+      writeRaster(img, filename = paste0(dir_out,"/", paste0(substr(name_img, 1, 21), ".tif")), format = "GTiff")
     }
-    setwd(outdir)
-    s<-stack(list.files(outdir, pattern = ".nc", full.names = T))
 
-    shp<-readOGR(system.file('extdata', paste0(region, ".shp"), package= 'cropDemand'))
-    area<-shp[sub_region,]
-    s<-crop(s, area)
-    s<-mask(s, area)
-
-    #unlink(paste0(normalizePath(tempdir), "/", dir(tempdir)), recursive = TRUE)
-    return(s)
-  } else {message("Please, pay attention. You should download the variable ppt or eto!")}
+    setwd(dir_out)
+    s<-stack(list.files(dir_out, pattern = ".tif$", full.names = T))
+    return(s)} else{message("Please, pay attention. You should download the variable ppt or eto!")}
 }
-
